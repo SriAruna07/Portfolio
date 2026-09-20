83bbb5be3aafd554f656f4953add7cfb0eb767e7
Sri Aruna <littlearunasri@gmail.com>
2026-09-20 13:25:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+document.querySelectorAll(".nav-links a").forEach(function(link) {
+
+    link.addEventListener("click", function(event) {
+
+        event.preventDefault();
+
+        const section = document.querySelector(
+            this.getAttribute("href")
+        );
+
+        section.scrollIntoView({
+            behavior: "smooth"
+        });
+
+    });
+
+});
+
+const contactForm = document.getElementById("contact-form");
+
+contactForm.addEventListener("submit", async function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    const response = await fetch("http://localhost:3000/contact", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+            message: message
+        })
+    });
+
+    const result = await response.json();
+
+    alert(result.message);
+
+    contactForm.reset();
+});
---
+++ server.js
@@ -0,0 +1,62 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = 3000;
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log("MongoDB connected successfully!"))
+    .catch((error) => console.log("MongoDB connection error:", error));
+
+    const contactSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    message: String,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Contact = mongoose.model("Contact", contactSchema);
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Portfolio backend is running!");
+});
+app.post("/contact", async (req, res) => {
+    try {
+        const { name, email, message } = req.body;
+
+        const newContact = new Contact({
+            name: name,
+            email: email,
+            message: message
+        });
+
+        await newContact.save();
+
+        console.log("Contact message saved to MongoDB!");
+
+        res.json({
+            message: "Your message was received successfully!"
+        });
+
+    } catch (error) {
+        console.log("Error saving contact message:", error);
+
+        res.status(500).json({
+            message: "Something went wrong."
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
+
